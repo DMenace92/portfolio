2871ac0da3f458abfb93e3e76fbe8b9446d885e7
DMenace92 <dennisenwiya@gmail.com>
2023-06-22 01:07:10 -0700

    new modifications

--- src/Components/PersonalInfoContainer/Skills/SkillsInfo.js
+++ src/Components/PersonalInfoContainer/Skills/SkillsInfo.js
@@ -5,6 +5,10 @@ import image1 from '../../../Images/javascript.png'
 import image2 from '../../../Images/java-script.png'
 import image3 from '../../../Images/mongodb.png'
 import image4 from '../../../Images/npm.png'
+import image5 from '../../../Images/angular.png'
+import image6 from '../../../Images/c.png'
+import image7 from '../../../Images/python.png'
+import image8 from '../../../Images/java.png'
 import './SkillInfo.css'
 
 //icons for skills https://iconscout.com/icons/javascript
@@ -30,6 +34,18 @@ const SkillInfo = () => {
                             </div>
                             <div className="imageHolder">
                                 <img className="imageDetailing" src={image4} alt="javascript"></img>
+                            </div>
+                            <div className="imageHolder">
+                                <img className="imageDetailing" src={image5} alt="javascript"></img>
+                            </div>
+                            <div className="imageHolder">
+                                <img className="imageDetailing" src={image6} alt="javascript"></img>
+                            </div>
+                            <div className="imageHolder">
+                                <img className="imageDetailing" src={image7} alt="javascript"></img>
+                            </div>
+                            <div className="imageHolder">
+                                <img className="imageDetailing" src={image8} alt="javascript"></img>
                             </div>
 
                         </div>
