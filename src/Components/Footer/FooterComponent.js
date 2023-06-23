50a2e8a5c59ba02d7482a91598f1bce931e385bf
DMenace92 <dennisenwiya@gmail.com>
2023-06-22 23:06:28 -0700

    new styles

--- src/Components/Footer/FooterComponent.js
+++ src/Components/Footer/FooterComponent.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import image from '../../Images/MyLogo1.jpg'
+import facebook from '../../Images/facebook.png'
+import instagram from '../../Images/instagram.png'
+import twitter from '../../Images/twitter.png'
+import linkedin from '../../Images/linkedin.png'
+import github from '../../Images/github.png'
 import './FooterComponent.css'
 
 const FooterComponent = () => {
@@ -9,10 +14,10 @@ const FooterComponent = () => {
                 <div className="footer__content">
                     <div className="footer__logo">
                         {/* Your logo here */}
-                        <img src={image} alt="Logo" />
+                        {/* <img src={image} alt="Logo" /> */}
                     </div>
                     <div className="footer__links">
-                        <ul className="footer__list">
+                        {/* <ul className="footer__list">
                             <li className="footer__list-item">
                                 <a href="/">Home</a>
                             </li>
@@ -25,25 +30,37 @@ const FooterComponent = () => {
                             <li className="footer__list-item">
                                 <a href="/contact">Contact</a>
                             </li>
-                        </ul>
+                        </ul> */}
                     </div>
-                    <div className="footer__social-media">
-                        {/* Add your social media icons or links here */}
-                        <a href="https://www.facebook.com">
-                            <i className="fab fa-facebook"></i>
-                        </a>
-                        <a href="https://www.twitter.com">
-                            <i className="fab fa-twitter"></i>
-                        </a>
-                        <a href="https://www.instagram.com">
-                            <i className="fab fa-instagram"></i>
-                        </a>
-                    </div>
+
                 </div>
             </div>
             <div className="footer__bottom">
                 <div className="container">
                     <p>&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
+                </div>
+                <div className="footer__social-media">
+                    {/* Add your social media icons or links here */}
+                    <a href="https://www.facebook.com">
+                        <i className="fab fa-facebook"></i>
+                        <img className="footerIcons" src={facebook}></img>
+                    </a>
+                    <a href="https://www.twitter.com">
+                        <i className="fab fa-twitter"></i>
+                        <img className="footerIcons" src={twitter}></img>
+                    </a>
+                    <a href="https://www.instagram.com">
+                        <i className="fab fa-instagram"></i>
+                        <img className="footerIcons" src={instagram}></img>
+                    </a>
+                    <a href="https://www.linkedin.com/in/dennisenwiya">
+                        <i className="fab fa-instagram"></i>
+                        <img className="footerIcons" src={linkedin}></img>
+                    </a>
+                    <a href="https://github.com/DMenace92">
+                        <i className="fab fa-twitter"></i>
+                        <img className="footerIcons" src={github}></img>
+                    </a>
                 </div>
             </div>
         </footer>
