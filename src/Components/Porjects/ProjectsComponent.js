93c7f029b0447651a312d9fae2fdf4a13fbe4f61
DMenace92 <dennisenwiya@gmail.com>
2023-08-24 11:09:54 -0700

    started with the project file

--- src/Components/Porjects/ProjectsComponent.js
+++ src/Components/Porjects/ProjectsComponent.js
@@ -1,17 +1,30 @@
 import { React } from 'react';
 import NavBar from '../NavBar/NavBarComponent'
 import commingSoon from '../../Images/commingsoon/25516.jpg';
+import { Grid, Paper } from '@mui/material'
 import './projectsComponent.css'
 
 const ProjectComponent = () => {
     return (
         <div>
             <NavBar />
-            <div className="projectsContainer">
+            {/* <div className="projectsContainer">
                 <img className="projectCommingSoon" src={commingSoon} ></img>
-            </div>
+            </div> */}
+            <Grid>
+                <div className="projectContainer">
+                    <Paper className="projectWrapper" elevation="24">
 
-        </div>
+                        <div>
+                            <img className="projectCommingSoon" src={commingSoon} ></img>
+
+
+                        </div>
+                    </Paper>
+                </div>
+            </Grid >
+
+        </div >
     )
 }
 export default ProjectComponent
