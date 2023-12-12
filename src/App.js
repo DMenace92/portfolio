1b2f3f553fc4a5518ee2c0526172eb0bcc615a27
Nathan McCellan <nathanmccellan@Nathans-MacBook-Air.local>
2023-12-11 20:36:18 -0700

    did things added notes, yay

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import MainPageComponent from './Components/MainProfolioPage/MainPageComponent'
-import AboutInfo from './Components/PersonalInfoContainer/About/AboutInfo';
-import Certification from './Components/PersonalInfoContainer/Certification/Certification'
-import Projects from './Components/Porjects/ProjectsComponent'
-import { Route, Routes } from 'react-router-dom'
+import MainPageComponent from "./Components/MainProfolioPage/MainPageComponent";
+import AboutInfo from "./Components/PersonalInfoContainer/About/AboutInfo";
+import Certification from "./Components/PersonalInfoContainer/Certification/Certification";
+import Projects from "./Components/Porjects/ProjectsComponent";
+import { Route, Routes } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        {/* TODO: Routes are no longer necessary, go ahead and remove the library and go with single page experience */}
         <Route path="/" element={<MainPageComponent />} />
         <Route path="/about" element={<AboutInfo />} />
         <Route path="/certifications" element={<Certification />} />
         <Route path="/projects" element={<Projects />} />
-
       </Routes>
     </div>
   );
--- src/Components/MainProfolioPage/MainPageComponent.js
+++ src/Components/MainProfolioPage/MainPageComponent.js
@@ -1,26 +1,25 @@
-import React from 'react'
-import NavBarComponent from '../NavBar/NavBarComponent'
-import Profile from '../Profile/ProfileComponent'
-import AboutInfo from '../PersonalInfoContainer/About/AboutInfo'
-import SkillInfo from '../PersonalInfoContainer/Skills/SkillsInfo'
+import React from "react";
+import NavBarComponent from "../NavBar/NavBarComponent";
+import Profile from "../Profile/ProfileComponent";
+import AboutInfo from "../PersonalInfoContainer/About/AboutInfo";
+import SkillInfo from "../PersonalInfoContainer/Skills/SkillsInfo";
 // import EmailForm from '../EmailForm/EmailForm'
-import EmailForm from '../../Containers/EmailContainer'
-import FooterComponent from '../Footer/FooterComponent'
-import './MainPageComponent.css'
+import EmailForm from "../../Containers/EmailContainer";
+import FooterComponent from "../Footer/FooterComponent";
+import "./MainPageComponent.css";
 //import { Grid, Paper } from '@material_ui/core'
 
 function MainPageComponent() {
-    return (
-        <div>
-            {/* <NavBarComponent /> */}
-            <div className="MainPageWrapper">
-                <Profile />
-                {/* <AboutInfo />
-                <SkillInfo />
-                <EmailForm /> */}
-                {/* <FooterComponent /> */}
-            </div>
-        </div>
-    )
+  return (
+    <div className="MainPageWrapper">
+      {/** TODO: Create your <Sidebar /> component here and pull
+       * side bar items into the new component from Profile */}
+      {/**
+       * TODO: Create <MainContent /> and pull everything from <Profile /> that isn't in the left side
+       * of the screen and bring it into this component delete Profile
+       */}
+      <Profile />
+    </div>
+  );
 }
-export default MainPageComponent
+export default MainPageComponent;
