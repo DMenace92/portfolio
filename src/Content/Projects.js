f93602bfdc9cf5102830c1156cf4158e28814eac
Nathan McCellan <nathanmccellan@Nathans-MacBook-Air.local>
2024-03-26 18:50:14 -0700

    added project cards

--- src/Content/Projects.js
+++ src/Content/Projects.js
@@ -1,26 +1,64 @@
 import React from 'react'
 import styles from './Projects.module.css'
-import arrow from '../Images/arrow.svg'
+import ProjectCard from '../Components/Projects/ProjectCard'
 
 const Projects = () => {
+  const projects = [
+    {
+      title: 'Dog App',
+      techUsed: ['javascript', 'typescript'],
+      links: [{ url: 'https://', label: 'www.decorative.link.com' }],
+      features: [
+        'user db search complete with custom api',
+        'user selection screen',
+        'user details view',
+      ],
+      image: {
+        link: 'https://www.volumetree.com/wp-content/uploads/2021/11/how-Rover-app-work.png',
+        description: 'this image is dog',
+      },
+      // video: { link: 'https://...', description: 'this is also dog' },
+      description:
+        'it is about dogs and stuffs doing dog things like sniffing and cat nomming. they loves to noms the cats unless the cats is all prickly and pokey. then the doggses cries and poops themselves some morz. is disgustinin',
+    },
+    {
+      title: 'Also Dog App 2',
+      techUsed: ['javascript', 'typescript'],
+      links: [{ url: 'https://', label: 'www.decorative.link.com' }],
+      features: [
+        'user db search complete with custom api',
+        'user selection screen',
+        'user details view',
+      ],
+      image: {
+        link: 'https://s3-alpha.figma.com/hub/file/2192153279/e6c39181-4c3a-49e0-b467-1b67412c70b1-cover.png',
+        description: 'this image is dog',
+      },
+      // video: { link: 'https://...', description: 'this is also dog' },
+      description: 'it is about dog',
+    },
+  ]
+
+  console.log('projects.length: ', projects.length)
+
   return (
     <div className={styles.ProjectMainContainer}>
       <div className={styles.ProjectBanner}>
         <h2>Projects</h2>
-      </div>
-      <div className={styles.MainImageHolderContainer}>
-        <div className={styles.projectImageHolderTest}>
-          <button className={styles.navigationImageButton}>
-            <img src={arrow}></img>
-          </button>
-          <div className={styles.projectImageHolder}>{/* carrocell */}</div>
-          <button className={styles.navigationImageButton}>
-            <img className={styles.arrowIconDesign} src={arrow}></img>
-          </button>
-        </div>
-        <div className={styles.ProjectTitleInfo}>
-          <h3 className={styles.projectTitleText}>Project Title</h3>
-        </div>
+        {projects.length > 0
+          ? projects.map((project) => (
+              <ProjectCard
+                key={project.title}
+                title={project.title}
+                techUsed={project.techUsed}
+                links={project.links}
+                features={project.features}
+                image={project.image}
+                video={project.video}
+                description={project.description}
+              />
+            ))
+          : null}
       </div>
     </div>
   )
